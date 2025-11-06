3944c1846184a5ef4bdf7f1da9dec33fed16a439
oscar-mugambi <oscarmugambi@gmail.com>
2025-11-06 16:49:47 +0300

    remove hardcoded http url

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,6 @@ FROM node:20-alpine AS builder
 WORKDIR /app
 RUN npm install -g pnpm
 
-ARG VITE_API_URL
-ENV VITE_API_URL=${VITE_API_URL:-http://localhost:5050/api/v1}  
 
 COPY package.json pnpm-lock.yaml ./
 RUN pnpm install --frozen-lockfile
--- src/axios.ts
+++ src/axios.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL || 'https://node.cosmicpenguin.xyz/api/v1',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
